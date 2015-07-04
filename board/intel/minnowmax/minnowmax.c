bb769d3467e71392a328381d1e02d6d0a8d83570
Simon Glass <sjg@chromium.org>
2015-07-04 13:58:20 -0600

    x86: Convert minnowmax to use CONFIG_DM_NET
    Move to driver model for networking on minnowmax.
    
    Change-Id: Ia49a11829be8b3344ddd0f203a044c64c02a076c
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- board/intel/minnowmax/minnowmax.c
+++ board/intel/minnowmax/minnowmax.c
@@ -8,7 +8,6 @@
 #include <asm/gpio.h>
 #include <asm/ibmpc.h>
 #include <asm/pnp_def.h>
-#include <netdev.h>
 #include <smsc_lpc47m.h>
 
 #define SERIAL_DEV PNP_DEV(0x2e, 4)
@@ -31,9 +30,4 @@ int board_early_init_f(void)
 void setup_pch_gpios(u16 gpiobase, const struct pch_gpio_map *gpio)
 {
 	return;
-}
-
-int board_eth_init(bd_t *bis)
-{
-	return pci_eth_init(bis);
 }
