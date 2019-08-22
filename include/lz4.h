1506585ed4f4f46fd7ecb6a4a6220df58f9fbd8d
Simon Glass <sjg@chromium.org>
2019-08-21 18:54:28 -0700

    common: Add a new lz4.h header file
    Add a header file to house the lz4 compression function.
    
    Change-Id: I543ab46bd15e7b763096b16f00713046f3217052
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ include/lz4.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2019 Google LLC
+ */
+
+#ifndef __LZ4_H
+#define __LZ4_H
+
+int ulz4fn(const void *src, size_t srcn, void *dst, size_t *dstn);
+
+#endif
