7ec1b9668eae060b3404b95063631981e0270aaa
Simon Glass <sjg@chromium.org>
2019-10-26 12:47:15 -0600

    power: Tidy up inclusion of regulator_common.h
    This file should not include common.h and dm.h so remove them. Also move
    the inclusion of this file to after the normal includes.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/power/regulator/fixed.c
+++ drivers/power/regulator/fixed.c
@@ -5,12 +5,13 @@
  *  Przemyslaw Marczak <p.marczak@samsung.com>
  */
 
-#include "regulator_common.h"
 #include <common.h>
 #include <errno.h>
 #include <dm.h>
 #include <power/pmic.h>
 #include <power/regulator.h>
+
+#include "regulator_common.h"
 
 static int fixed_regulator_ofdata_to_platdata(struct udevice *dev)
 {
