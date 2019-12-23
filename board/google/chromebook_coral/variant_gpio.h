fd049dd05730249200018cb324fb9d8bd431393a
Simon Glass <sjg@chromium.org>
2019-12-23 15:34:33 -0700

    ok
    Change-Id: I59ea11105e15513fbcb27be1a0acc52c3a9c799f

--- board/google/chromebook_coral/variant_gpio.h
+++ board/google/chromebook_coral/variant_gpio.h
@@ -51,4 +51,20 @@
 /* DMIC_CONFIG_PIN: High for 1-DMIC and low for 4-DMIC's */
 #define DMIC_CONFIG_PIN	GPIO_17
 
+#ifndef __ASSEMBLY__
+
+enum cros_gpio_t {
+	CROS_GPIO_REC = 1, /* Recovery */
+	CROS_GPIO_DEPRECATED_DEV = 2, /* Developer;
+				       * deprecated (chromium:942901) */
+	CROS_GPIO_WP = 3, /* Write Protect */
+	CROS_GPIO_PE = 4, /* Phase enforcement for final product */
+
+	CROS_GPIO_ACTIVE_LOW = 0,
+	CROS_GPIO_ACTIVE_HIGH = 1,
+
+	CROS_GPIO_VIRTUAL = -1,
+};
+#endif
+
 #endif /* BASEBOARD_GPIO_H */
