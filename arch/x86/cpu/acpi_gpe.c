9b4f70d752a405d62c5d1d9b7358e13ee7c96e5a
Simon Glass <sjg@chromium.org>
2020-01-19 18:22:51 +1300

    x86: irq: Support flags for acpi_gpe
    This binding currently has a flags cell but it is not used. Make use of it
    to create ACPI tables for interrupts.
    
    Change-Id: Ib4031b53044900874165fc11e273f5e2571306c4
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/x86/cpu/acpi_gpe.c
+++ arch/x86/cpu/acpi_gpe.c
@@ -5,9 +5,12 @@
  */
 
 #include <common.h>
+#include <acpi_device.h>
 #include <dm.h>
 #include <irq.h>
 #include <asm/io.h>
+#include <dt-bindings/interrupt-controller/irq.h>
+#include <dt-bindings/interrupt-controller/x86-irq.h>
 
 struct acpi_gpe_priv {
 	ulong acpi_base;
@@ -56,6 +59,24 @@ static int acpi_gpe_ofdata_to_platdata(struct udevice *dev)
 static int acpi_gpe_of_xlate(struct irq *irq, struct ofnode_phandle_args *args)
 {
 	irq->id = args->args[0];
+	irq->flags = args->args[1];
+
+	return 0;
+}
+
+static int acpi_gpe_get_acpi(const struct irq *irq, struct acpi_irq *acpi_irq)
+{
+	memset(acpi_irq, '\0', sizeof(*acpi_irq));
+	acpi_irq->pin = irq->id;
+	acpi_irq->mode = irq->flags & IRQ_TYPE_EDGE_BOTH ?
+		ACPI_IRQ_EDGE_TRIGGERED : ACPI_IRQ_LEVEL_TRIGGERED;
+	acpi_irq->polarity = irq->flags &
+		 (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_LEVEL_LOW) ?
+		 ACPI_IRQ_ACTIVE_LOW : ACPI_IRQ_ACTIVE_HIGH;
+	acpi_irq->shared = irq->flags & X86_IRQ_TYPE_SHARED ?
+		ACPI_IRQ_SHARED : ACPI_IRQ_EXCLUSIVE;
+	acpi_irq->wake = irq->flags & X86_IRQ_TYPE_WAKE ? ACPI_IRQ_WAKE :
+		ACPI_IRQ_NO_WAKE;
 
 	return 0;
 }
@@ -63,6 +84,7 @@ static int acpi_gpe_of_xlate(struct irq *irq, struct ofnode_phandle_args *args)
 static const struct irq_ops acpi_gpe_ops = {
 	.read_and_clear		= acpi_gpe_read_and_clear,
 	.of_xlate		= acpi_gpe_of_xlate,
+	.get_acpi		= acpi_gpe_get_acpi,
 };
 
 static const struct udevice_id acpi_gpe_ids[] = {
