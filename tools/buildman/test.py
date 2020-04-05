5a8061739f0f7207d15192a35bf8cd2c6bbba54e
Simon Glass <sjg@chromium.org>
2020-04-04 21:16:26 -0600

    buildman: Add an option to ignore device-tree warnings
    Unfortunately the plague of device-tree warnings has not lifted. These
    warnings infiltrate almost every build, adding noise and confusion.
    
    Add a buildman option to ignore them. This option works only with the
    summary option (-s). It does not affect the build process.
    
    Series-to: u-boot
    Series-cc: trini, bin, Cristian Ciocaltea <cristian.ciocaltea@gmail.com>
    Series-cc: stephen
    Cover-letter:
    buildman: Improve summary output
    This series makes the output produced by buildman a little more useful and
    easier to read:
    
    - when using -l to see which errors relate to which board, it shows the
       boards in a different colour so they can be distinguished from the
       errors
    - uses yellow consistently for warning lines
    - shows the number of remaining builds
    - shows a summary when the build completes, including the time taken
    - adds a few messages to indicate what is happening while starting up
    
    It also refactors a few tests a little to make them easier to follow,
    makes -I the default and improves the documentation a little.
    END
    
    Change-Id: I1a7849b8623c5083ff547814647e5e86022bcec4
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/buildman/test.py
+++ tools/buildman/test.py
@@ -214,13 +214,15 @@ class TestBuild(unittest.TestCase):
             terminal.EchoPrintTestLines()
         return iter(terminal.GetPrintTestLines())
 
-    def _CheckOutput(self, lines, list_error_boards):
+    def _CheckOutput(self, lines, list_error_boards, filter_dtb_warnings):
         """Check for expected output from the build summary
 
         Args:
             lines: Iterator containing the lines returned from the summary
             list_error_boards: Adjust the check for output produced with the
                --list-error-boards flag
+            filter_dtb_warnings: Adjust the check for output produced with the
+               --filter-dtb-warnings flag
         """
         def add_line_prefix(prefix, boards, error_str, colour):
             """Add a prefix to each line of a string
@@ -306,9 +308,11 @@ class TestBuild(unittest.TestCase):
         self.assertEqual(line.text,
                          add_line_prefix('-', boards234, errors[1], col.GREEN))
 
-        line = next(lines)
-        self.assertEqual(line.text,
-                         add_line_prefix('w+', boards34, errors[2], col.YELLOW))
+        if not filter_dtb_warnings:
+            line = next(lines)
+            self.assertEqual(
+                line.text,
+                 add_line_prefix('w+', boards34, errors[2], col.YELLOW))
 
         # Fifth commit
         self.assertEqual(next(lines).text, '05: %s' % commits[4][1])
@@ -324,9 +328,11 @@ class TestBuild(unittest.TestCase):
         self.assertEqual(line.text,
                          add_line_prefix('+', boards4, expect, col.RED))
 
-        line = next(lines)
-        self.assertEqual(line.text,
-                         add_line_prefix('w-', boards34, errors[2], col.CYAN))
+        if not filter_dtb_warnings:
+            line = next(lines)
+            self.assertEqual(
+                line.text,
+                add_line_prefix('w-', boards34, errors[2], col.CYAN))
 
         # Sixth commit
         self.assertEqual(next(lines).text, '06: %s' % commits[5][1])
@@ -370,7 +376,8 @@ class TestBuild(unittest.TestCase):
         This does a line-by-line verification of the summary output.
         """
         lines = self._SetupTest(show_errors=True)
-        self._CheckOutput(lines, list_error_boards=False)
+        self._CheckOutput(lines, list_error_boards=False,
+                          filter_dtb_warnings=False)
 
     def testErrorBoards(self):
         """Test output with --list-error-boards
@@ -378,7 +385,17 @@ class TestBuild(unittest.TestCase):
         This does a line-by-line verification of the summary output.
         """
         lines = self._SetupTest(show_errors=True, list_error_boards=True)
-        self._CheckOutput(lines, list_error_boards=True)
+        self._CheckOutput(lines, list_error_boards=True,
+                          filter_dtb_warnings=False)
+
+    def testFilterDtb(self):
+        """Test output with --filter-dtb-warnings
+
+        This does a line-by-line verification of the summary output.
+        """
+        lines = self._SetupTest(show_errors=True, filter_dtb_warnings=True)
+        self._CheckOutput(lines, list_error_boards=False,
+                          filter_dtb_warnings=True)
 
     def _testGit(self):
         """Test basic builder operation by building a branch"""
