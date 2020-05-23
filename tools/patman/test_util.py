6da3f4bb04e540f40841274c8330977a0e61b27a
Simon Glass <sjg@chromium.org>
2020-05-23 09:46:40 -0600

    binman: Re-enable concurrent tests
    With the change to absolute imports the concurrent tests feature
    unfortunately broke. Fix it.
    
    We cannot easy add a warning, since the output messes up tests which check
    the output.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I6d9ab323720e230ba651cb6f734835b19238fbd4

--- tools/patman/test_util.py
+++ tools/patman/test_util.py
@@ -17,7 +17,8 @@ from io import StringIO
 
 use_concurrent = True
 try:
-    from concurrencytest import ConcurrentTestSuite, fork_for_tests
+    from concurrencytest.concurrencytest import ConcurrentTestSuite
+    from concurrencytest.concurrencytest import fork_for_tests
 except:
     use_concurrent = False
 
@@ -51,6 +52,7 @@ def RunTestCoverage(prog, filter_fname, exclude_list, build_dir, required=None,
         glob_list = []
     glob_list += exclude_list
     glob_list += ['*libfdt.py', '*site-packages*', '*dist-packages*']
+    glob_list += ['*concurrencytest*']
     test_cmd = 'test' if 'binman' in prog else '-t'
     prefix = ''
     if build_dir:
