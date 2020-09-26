f154f2038c0e70a5bde131752be30d521add239a
Simon Glass <sjg@chromium.org>
2020-09-26 16:13:10 -0600

    bootm: Update fixup_silent_linux() to return an error
    At present this function fails silently on error. Update it to produce
    an error code. Report this error to the user and abort the boot, since it
    likely will prevent a successful start.
    
    No tests are added at this stage, since additional refactoring is taking
    place in subsequent patches.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/bootm.h
+++ include/bootm.h
@@ -75,7 +75,14 @@ void board_quiesce_devices(void);
  */
 void switch_to_non_secure_mode(void);
 
-/* Adjust the 'bootargs' to ensure that Linux boots silently, if required */
-void fixup_silent_linux(void);
+/**
+ * fixup_silent_linux() - Process fix-ups for the command line
+ *
+ * Updates the 'bootargs' envvar as required. This handles making Linux boot
+ * silently if requested ('silent_linux' envvar)
+ *
+ * @return 0 if OK, -ENOMEM if out of memory
+ */
+int fixup_silent_linux(void);
 
 #endif
