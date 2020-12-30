9864ca6b1eba8799f8f866936494e585dcd9b25e
Simon Glass <sjg@chromium.org>
2020-12-30 05:21:50 -0700

    dm: core: Create a struct for device runtime info
    At present when driver model needs to change a device it simply updates
    the struct udevice structure. But with of-platdata-inst most of the fields
    are not modified at runtime. In fact, typically only the flags need to
    change.
    
    For systems running SPL from read-only memory it is convenient to separate
    out the runtime information, so that the devices don't need to be copied
    before being used.
    
    Create a new udevice_rt table, similar to the existing driver_rt. For now
    it just holds the flags, although they are not used in this patch.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I6420b248f58963ecf5c57a67b084d782478ae8c8

--- include/asm-generic/global_data.h
+++ include/asm-generic/global_data.h
@@ -219,6 +219,10 @@ struct global_data {
 	/** @dm_driver_rt: Dynamic info about the driver */
 	struct driver_rt *dm_driver_rt;
 # endif
+#if CONFIG_IS_ENABLED(OF_PLATDATA_RT)
+	/** @dm_udevice_rt: Dynamic info about the udevice */
+	struct udevice_rt *dm_udevice_rt;
+# endif
 #endif
 #ifdef CONFIG_TIMER
 	/**
@@ -477,6 +481,14 @@ struct global_data {
 #else
 #define gd_set_dm_driver_rt(dyn)
 #define gd_dm_driver_rt()		NULL
+#endif
+
+#if CONFIG_IS_ENABLED(OF_PLATDATA_RT)
+#define gd_set_dm_udevice_rt(dyn)	gd->dm_udevice_rt = dyn
+#define gd_dm_udevice_rt()		gd->dm_udevice_rt
+#else
+#define gd_set_dm_udevice_rt(dyn)
+#define gd_dm_udevice_rt()		NULL
 #endif
 
 #ifdef CONFIG_GENERATE_ACPI_TABLE
