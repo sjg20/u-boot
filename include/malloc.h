162de8fb1a825b9cb19d824b8bf88adacf9096a0
Simon Glass <sjg@chromium.org>
2021-02-10 20:40:09 -0700

    malloc: Export malloc_simple_info()
    Export this function always so it can be used behind IS_ENABLED() isntead
    of requiring an #ifdef.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/malloc.h
+++ include/malloc.h
@@ -877,6 +877,8 @@ extern Void_t*     sbrk();
 
 #else
 
+void malloc_simple_info(void);
+
 #if CONFIG_IS_ENABLED(SYS_MALLOC_SIMPLE)
 #define malloc malloc_simple
 #define realloc realloc_simple
@@ -884,7 +886,6 @@ extern Void_t*     sbrk();
 static inline void free(void *ptr) {}
 void *calloc(size_t nmemb, size_t size);
 void *realloc_simple(void *ptr, size_t size);
-void malloc_simple_info(void);
 #else
 
 # ifdef USE_DL_PREFIX
