764543fb922c62707ea71091aacafff05e375887
Simon Glass <sjg@chromium.org>
2021-02-10 20:40:47 -0700

    test: Pending upstream change for SPL tests
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/dm/test-main.c
+++ test/dm/test-main.c
@@ -30,7 +30,7 @@ static int dm_test_init(struct unit_test_state *uts, bool of_live)
 
 	memset(dms, '\0', sizeof(*dms));
 	gd->dm_root = NULL;
-	if (!CONFIG_IS_ENABLED(OF_PLATDATA))
+	if (!IS_ENABLED(SPL_BUILD))
 		memset(dm_testdrv_op_count, '\0', sizeof(dm_testdrv_op_count));
 	state_reset_for_test(state_get_current());
 
