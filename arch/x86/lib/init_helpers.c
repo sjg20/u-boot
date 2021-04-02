8c3badb2c46f029c42cb8d1966deb8700fff1ba7
Simon Glass <sjg@chromium.org>
2021-04-02 18:09:00 +1300

    x86: Do cache set-up by default when booting from coreboot
    A recent change to disable cache setup when booting from coreboot
    assumed that this has been done by SPL. The result is that for the
    coreboot board, the cache is disabled (in start.S) and never
    re-enabled.
    
    If the cache was turned off, as it is on boards without SPL, we should
    turn it back on. Add this new condition.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Change-Id: I3985bbff0c8e8da9bbec97b4e740de82e4bf2e51

--- arch/x86/lib/init_helpers.c
+++ arch/x86/lib/init_helpers.c
@@ -18,10 +18,7 @@ int init_cache_f_r(void)
 		 IS_ENABLED(CONFIG_FSP_VERSION2);
 	int ret;
 
-	if (!ll_boot_init())
-		return 0;
-
-	do_mtrr &= !IS_ENABLED(CONFIG_FSP_VERSION1) &&
+	do_mtrr &= !IS_ENABLED(CONFIG_SPL) && !IS_ENABLED(CONFIG_FSP_VERSION1) &&
 		!IS_ENABLED(CONFIG_SYS_SLIMBOOTLOADER);
 
 	if (do_mtrr) {
