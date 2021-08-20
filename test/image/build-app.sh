23dd94ff62848ba1f1672d45d9366c4a2912618f
Simon Glass <sjg@chromium.org>
2021-08-20 10:09:39 -0600

    script
    Change-Id: I7f72732a76627109b76209f5ec623774fc8b5711

---
+++ test/image/build-app.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Script to build an EFI payload
+# Just for example
+
+TMP=/tmp/efi32
+MNT=/mnt/x
+BUILD=efi-x86_app
+
+qemu-img create try.img 20M
+mkfs.vfat try.img
+mkdir -p $TMP
+cat >$TMP/startup.nsh <<EOF
+fs0:u-boot-app.efi
+EOF
+sudo cp /tmp/b/$BUILD/u-boot-app.efi $TMP
+sudo cp /boot/vmlinuz-5.4.0-77-generic $TMP/vmlinuz
+
+sudo mount -o loop try.img $MNT
+sudo cp $TMP/* $MNT
+sudo umount $MNT
