3b85457f0650d795973b8e116fece4865e082dce
Simon Glass <sjg@chromium.org>
2021-09-03 05:11:38 -0600

    new-script
    Change-Id: Ic4d32c355c4baf5745ba07ba17c04d0726791618

---
+++ test/image/build-efi.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Script to build an EFI thing suitable for booting with qemu
+# Just for example
+
+set -e
+
+usage() {
+	echo "Usage: $0 [-a | -p]" 1>&2
+	echo 1>&2
+	echo "   -a   - Package up the app" 1>&2
+	echo "   -o   - Use old EFI app build (before 32/64 split)" 1>&2
+	echo "   -p   - Package up the payload" 1>&2
+	echo "   -r   - Run qemu with the image" 1>&2
+	echo "   -w   - Use word version (32-bit)" 1>&2
+	exit 1
+}
+
+# 32- or 64-bit EFI
+bitness=64
+
+# app or payload ?
+type=app
+
+# run the image with qemu
+run=
+
+# before the 32/64 split of the app
+old=
+
+while getopts "aoprw" opt; do
+	case "${opt}" in
+	a)
+		type=app
+		;;
+	p)
+		type=payload
+		;;
+	r)
+		run=1
+		;;
+	w)
+		bitness=32
+		;;
+	o)
+		old=1
+		;;
+	*)
+		usage
+		;;
+	esac
+done
+
+run_qemu() {
+	if [[ "${bitness}" = "64" ]]; then
+		qemu=qemu-system-x86_64
+		bios=OVMF-pure-efi.x64.fd
+	else
+		qemu=qemu-system-i386
+		bios=OVMF-pure-efi.i386.fd
+	fi
+	"${qemu}" -bios "${bios}" \
+		-drive id=disk,file=try.img,if=none,format=raw \
+		-nic none -device ahci,id=ahci \
+		-device ide-hd,drive=disk,bus=ahci.0
+}
+
+TMP="/tmp/efi${bitness}${type}"
+MNT=/mnt/x
+BUILD="efi-x86_${type}${bitness}"
+
+if [[ -n "${old}" && "${bitness}" = "32" ]]; then
+	BUILD="efi-x86_${type}"
+fi
+
+echo "Packaging ${BUILD}"
+qemu-img create try.img 24M >/dev/null
+mkfs.vfat try.img >/dev/null
+mkdir -p $TMP
+cat >$TMP/startup.nsh <<EOF
+fs0:u-boot-${type}.efi
+EOF
+sudo cp /tmp/b/$BUILD/u-boot-${type}.efi $TMP
+#sudo cp /tmp/b/$BUILD/image.bin $TMP/chromeos.rom
+#sudo cp /boot/vmlinuz-5.4.0-77-generic $TMP/vmlinuz
+
+sudo mount -o loop try.img $MNT
+sudo cp $TMP/* $MNT
+sudo umount $MNT
+
+if [[ -n "${run}" ]]; then
+	run_qemu
+fi
