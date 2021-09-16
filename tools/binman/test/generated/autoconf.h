30783dfaf3e5f97415b37955437df55add467051
Simon Glass <sjg@chromium.org>
2021-09-16 06:04:30 -0600

    kconfig: Update CONFIG_IS_ENABLED() for split files
    Update this file to include the correct autoconf.h or autoconf_spl.h file
    for each phase. This allows the macros to be simplified.
    
    With this, CONFIG_IS_ENABLED() is the same as IS_ENABLED() apart from a
    migration detail.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/test/generated/autoconf.h
+++ tools/binman/test/generated/autoconf.h
@@ -1,3 +1,5 @@
 #define CONFIG_BINMAN 1
 #define CONFIG_SPL_BUILD 1
-#define CONFIG_SPL_BINMAN_SYMBOLS 1
+
+/* Don't include the SPL_ here since we are using separate autoconf files */
+#define CONFIG_BINMAN_SYMBOLS 1
