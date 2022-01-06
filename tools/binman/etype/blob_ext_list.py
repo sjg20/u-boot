2dd4b10f1f6cd4d4e11580a23dda6cca7aa0476b
Simon Glass <sjg@chromium.org>
2022-01-06 06:25:26 -0700

    binman: Allow faked blobs in blob-ext-list
    Since this is a list of blobs, each blob should have the ability to be
    faked, as with blob-ext. Update the Entry base class to set allow_fake
    and use this in the section code also, so that this propagagtes to
    blob-ext-list, which is not a section.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/etype/blob_ext_list.py
+++ tools/binman/etype/blob_ext_list.py
@@ -37,6 +37,7 @@ class Entry_blob_ext_list(Entry_blob):
         missing = False
         pathnames = []
         for fname in self._filenames:
+            fname = self.CheckFakeBlob(fname)
             pathname = tools.GetInputFilename(
                 fname, self.external and self.section.GetAllowMissing())
             # Allow the file to be missing
