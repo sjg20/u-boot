6606bcd5dc5054e5dc56a15de4d240ec78ad3c78
Simon Glass <sjg@chromium.org>
2022-08-05 21:31:05 -0600

    blk: Drop if_type
    Use the uclass ID instead.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/efi_loader.h
+++ include/efi_loader.h
@@ -156,7 +156,7 @@ extern bool efi_st_keep_devices;
 
 /* EFI system partition */
 extern struct efi_system_partition {
-	enum if_type if_type;
+	enum uclass_id if_type;
 	int devnum;
 	u8 part;
 } efi_system_partition;
