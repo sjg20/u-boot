edb5a0b9771fddcbe7438e6261decb275ec72c5a
Simon Glass <sjg@chromium.org>
2022-11-28 15:51:45 +1300

    bootstd: Record the bootdevs used during scanning
    Add a way to record the bootdevs used when scanning for bootflows. This is
    useful for testing.
    
    Re-enable and update the affected tests now that we have this feature.
    
    For bootdev_test_order_default() there is no-longer any support for using
    the bootdev aliases to specify an ordering, so drop that part of the test.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- boot/bootflow.c
+++ boot/bootflow.c
@@ -88,6 +88,9 @@ void bootflow_iter_init(struct bootflow_iter *iter, int flags)
 	memset(iter, '\0', sizeof(*iter));
 	iter->first_glob_method = -1;
 	iter->flags = flags;
+
+	/* remember the first bootdevs we see */
+	iter->max_devs = BOOTFLOW_MAX_USED_DEVS;
 }
 
 void bootflow_iter_uninit(struct bootflow_iter *iter)
@@ -130,6 +133,10 @@ static void bootflow_iter_set_dev(struct bootflow_iter *iter,
 		  dev ? dev->name : "(none)", method_flags);
 	iter->dev = dev;
 	iter->method_flags = method_flags;
+
+	/* record the device for later */
+	if (dev && iter->num_devs < iter->max_devs)
+		iter->dev_used[iter->num_devs++] = dev;
 
 	if ((iter->flags & (BOOTFLOWF_SHOW | BOOTFLOWF_SINGLE_DEV)) ==
 	    BOOTFLOWF_SHOW) {
