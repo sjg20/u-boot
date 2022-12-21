1e6813ad69de4858ed864d47b9ab12093c56d28f
Simon Glass <sjg@chromium.org>
2022-12-21 06:02:17 -0700

    rockchip: Enable bootstage on rockpro64
    This board is useful for benchmarking overall U-Boot performance. Enable
    the bootstage feature so we get a report.
    
    Since this returns to the boot rom before finishing executing
    board_init_r() in SPL, add a few bootstage calls so that we can collect
    timing from TPL.
    
    For the stash region, use a portion of SRAM, 64KB below the stack top.
    This allows the TPL image to be up to nearly 120KB (it is typically about
    64KB). SPL normally runs from SDRAM at 0, so can use the same stash
    region.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -910,7 +910,7 @@ void preloader_console_init(void)
 	gd->have_console = 1;
 
 #if CONFIG_IS_ENABLED(BANNER_PRINT)
-	puts("\nU-Boot " SPL_TPL_NAME " " PLAIN_VERSION " (" U_BOOT_DATE " - "
+	puts("\nxU-Boot " SPL_TPL_NAME " " PLAIN_VERSION " (" U_BOOT_DATE " - "
 	     U_BOOT_TIME " " U_BOOT_TZ ")\n");
 #endif
 #ifdef CONFIG_SPL_DISPLAY_PRINT
