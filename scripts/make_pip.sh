5114a4eb895a5b95248d176e0165d746dad5aea0
Simon Glass <sjg@chromium.org>
2023-02-17 21:12:34 -0700

    wip

--- scripts/make_pip.sh
+++ scripts/make_pip.sh
@@ -16,20 +16,27 @@ cp -v tools/${tool}/pyproject.toml ${dir}
 cp -v Licenses/gpl-2.0.txt ${dir}/LICENSE
 readme="tools/${tool}/README.*"
 
-cat ${readme} | sed 's/:doc:`.*`//; /sectionauthor/d' > ${dir}/$(basename ${readme})
+cat ${readme} | sed -E 's/:(doc|ref):`.*`//; /sectionauthor/d; /toctree::/d' > ${dir}/$(basename ${readme})
 
 mkdir -p ${dir}/src/${tool}
-cp -v tools/$tool/*.py ${dir}/src/${tool}
-for subdir in $(find tools/${tool} -type d -maxdepth 1 | grep -v __pycache__); do
-	echo "copy ${subdir}"
+cp -v tools/$tool/*.py ${dir}/src
+pushd tools/${tool}
+for subdir in $(find . -maxdepth 1 -type d | grep -vE "(__pycache__|home|usr|scratch|\.$|pyproject)"); do
+	pathname="${dir}/src/${subdir}"
+	echo "Copy ${pathname}"
+	cp -a ${subdir} ${pathname}
 done
+popd
+find ${dir}/src -name __pycache__ -type f -exec rm {} \;
+find ${dir}/src -depth -name __pycache__ -exec rmdir {} \;
 mkdir ${dir}/tests
 cd ${dir}
 
 python3 -m pip install --upgrade build
 python3 -m pip install --upgrade twine
 python3 -m build
+
+echo "Uploading from ${dir}"
 python3 -m twine upload --repository testpypi -u __token__ dist/*
 
 echo "Completed build and upload of ${tool}"
-echo
