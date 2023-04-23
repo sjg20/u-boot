29ee55872e1c7b0c3c56da68b18bf0fee8fa3a94
Simon Glass <sjg@chromium.org>
2023-04-23 17:29:12 -0600

    test: Avoid strange symbols in the assembler file
    This works correctly on Linux with ELF but not on Windows with PE, since
    it creates assembly symbols with invalid names.
    
    Use the lowest/highest valid characters instead. This should still work
    correctly, since we have no tests starting with $ and none starting with
    'zz' at present.
    
    Series-changes: 2
    - Update comment and use zz to make it less likely we have a problem
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/test/test.h
+++ include/test/test.h
@@ -126,9 +126,9 @@ struct unit_test {
 #define UNIT_TEST_SUITE_COUNT(_suite) \
 	ll_entry_count(struct unit_test, ut_ ## _suite)
 
-/* Use ! and ~ so that all tests will be sorted between these two values */
-#define UNIT_TEST_ALL_START()	ll_entry_start(struct unit_test, ut_!)
-#define UNIT_TEST_ALL_END()	ll_entry_start(struct unit_test, ut_~)
+/* Use $ and zz so that all tests will be sorted between these two values */
+#define UNIT_TEST_ALL_START()	ll_entry_start(struct unit_test, ut_$)
+#define UNIT_TEST_ALL_END()	ll_entry_start(struct unit_test, ut_zz)
 #define UNIT_TEST_ALL_COUNT()	(UNIT_TEST_ALL_END() - UNIT_TEST_ALL_START())
 
 /* Sizes for devres tests */
