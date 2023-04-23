6b3e232ac5c3a460dbaf6112308d1107a5bf0a00
Simon Glass <sjg@chromium.org>
2023-04-23 17:33:20 -0600

    sandbox: Fix up setting of monitor_len on MSYS2
    The required linker symbols are not present. For now just return 0 in
    this case.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- common/board_f.c
+++ common/board_f.c
@@ -290,7 +290,7 @@ static int setup_mon_len(void)
 {
 #if defined(__ARM__) || defined(__MICROBLAZE__)
 	gd->mon_len = (ulong)&__bss_end - (ulong)_start;
-#elif defined(CONFIG_SANDBOX) && !defined(__riscv)
+#elif defined(CONFIG_SANDBOX) && !defined(__riscv) && !defined(__MSYS__)
 	gd->mon_len = (ulong)&_end - (ulong)_init;
 #elif defined(CONFIG_SANDBOX)
 	/* gcc does not provide _init in crti.o on RISC-V */
