e62126c3ae4541c4981fbba696377c78c50debd3
Simon Glass <sjg@chromium.org>
2023-05-22 18:23:01 -0600

    wip

--- boot/expo_build.c
+++ boot/expo_build.c
@@ -102,7 +102,7 @@ int add_expo_str(void *ldtb, struct expo *exp, const char *find_name)
  * @scn: Scene to add to
  * @find_name: Name to look for (e.g. "title"). This will find a property called
  * "title" if it exists, else will look up the string for "title-id"
- * Return: Id of added string, or -ve on error
+ * Return: ID of added string, or -ve on error
  */
 int add_txt_str(struct build_info *info, ofnode node, struct scene *scn,
 		const char *find_name, uint obj_id)
@@ -118,6 +118,53 @@ int add_txt_str(struct build_info *info, ofnode node, struct scene *scn,
 
 		snprintf(name, sizeof(name), "%s-id", find_name);
 		ret = ofnode_read_u32(node, name, &id);
+		if (ret)
+			return log_msg_ret("id", -EINVAL);
+
+		if (id >= info->str_count)
+			return log_msg_ret("id", -E2BIG);
+		text = info->str_for_id[id];
+		if (!text)
+			return log_msg_ret("id", -EINVAL);
+	}
+
+	ret = expo_str(scn->expo, find_name, 0, text);
+	if (ret < 0)
+		return log_msg_ret("add", ret);
+	str_id = ret;
+
+	ret = scene_txt_str(scn, find_name, obj_id, str_id, text, NULL);
+	if (ret < 0)
+		return log_msg_ret("add", ret);
+
+	return ret;
+}
+
+/**
+ * add_txt_str_list - Add a list string or lookup its ID, then add to expo
+ *
+ * @info: Build information
+ * @node: Node describing scene
+ * @scn: Scene to add to
+ * @find_name: Name to look for (e.g. "title"). This will find a string-list
+ * property called "title" if it exists, else will look up the string in the
+ * "title-id" string list.
+ * Return: ID of added string, or -ve on error
+ */
+int add_txt_str_list(struct build_info *info, ofnode node, struct scene *scn,
+		     const char *find_name, int index, uint obj_id)
+{
+	const char *text;
+	uint str_id;
+	int ret;
+
+	ret = ofnode_read_string_index(node, find_name, index, &text);
+	if (ret) {
+		char name[40];
+		u32 id;
+
+		snprintf(name, sizeof(name), "%s-id", find_name);
+		ret = ofnode_read_u32_index(node, name, index, &id);
 		if (ret)
 			return log_msg_ret("id", -EINVAL);
 
@@ -205,8 +252,9 @@ static int menu_build(struct build_info *info, ofnode node, struct scene *scn)
 	struct scene_obj_menu *menu;
 	const char *name;
 	uint title_id, menu_id;
+	int ret, index, size, i;
+	const u32 *item_ids;
 	u32 id;
-	int ret;
 
 	name = ofnode_get_name(node);
 	ret = ofnode_read_u32(node, "id", &id);
@@ -225,10 +273,28 @@ static int menu_build(struct build_info *info, ofnode node, struct scene *scn)
 	title_id = ret;
 	ret = scene_menu_set_title(scn, menu_id, title_id);
 
+	item_ids = ofnode_read_prop(node, "item-id", &size);
+	if (!item_ids)
+		return log_msg_ret("itm", -EINVAL);
+	if (!size || size % sizeof(u32))
+		return log_msg_ret("isz", -EINVAL);
+	size /= sizeof(u32);
 
+	for (i = 0; i < size; i++) {
+		struct scene_menitem *item;
+		uint key, label, desc;
 
-// 	id = scene_menuitem(scn, menu_id, "linux", ITEM1, ITEM1_KEY,
-// 			    ITEM1_LABEL, ITEM1_DESC, ITEM1_PREVIEW, 0, &item);
+		ret = add_txt_str_list(info, node, scn, "item-label", i, 0);
+		if (ret && ret != -ENOENT)
+			return log_msg_ret("lab", ret);
+		label = max(0, ret);
+
+		ret = scene_menuitem(scn, menu_id, simple_xtoa(index),
+				     item_ids[i], key, label, desc, 0, 0,
+				     &item);
+		if (ret < 0)
+			return log_msg_ret("mi", ret);
+	}
 
 	return 0;
 }
@@ -253,7 +319,7 @@ static int item_build(struct build_info *info, ofnode node, struct scene *scn)
 		ret = -EINVAL;
 	}
 	if (ret)
-		return log_msg_ret("typ", ret);
+		return log_msg_ret("bld", ret);
 
 	return 0;
 }
@@ -289,7 +355,7 @@ static int scene_build(struct build_info *info, ofnode scn_node,
 	ofnode_for_each_subnode(node, scn_node) {
 		ret = item_build(info, node, scn);
 		if (ret < 0)
-			return log_msg_ret("itm", ret);
+			return log_msg_ret("mit", ret);
 	}
 
 	return 0;
