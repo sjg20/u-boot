ae669b24b628f81ed3e1d6aac58494ccd45afdc8
Simon Glass <sjg@chromium.org>
2023-07-01 22:12:11 +0100

    buildman: Move code to remove old outputs
    Put this in its own function to reduce the size of the run_commit()
    function.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/buildman/builderthread.py
+++ tools/buildman/builderthread.py
@@ -283,6 +283,22 @@ class BuilderThread(threading.Thread):
                     will_build = False
         return will_build
 
+    def _remove_old_outputs(self, out_dir):
+        """Remove any old output-target files
+
+        Args:
+            out_dir (str): Output directory for the build
+
+        Since we use a build directory that was previously used by another
+        board, it may have produced an SPL image. If we don't remove it (i.e.
+        see do_config and self.mrproper below) then it will appear to be the
+        output of this build, even if it does not produce SPL images.
+        """
+        for elf in BASE_ELF_FILENAMES:
+            fname = os.path.join(out_dir, elf)
+            if os.path.exists(fname):
+                os.remove(fname)
+
     def run_commit(self, commit_upto, brd, work_dir, do_config, config_only,
                   force_build, force_build_failures, work_in_output,
                   adjust_cfg):
@@ -365,15 +381,7 @@ class BuilderThread(threading.Thread):
                 config_args = [f'{brd.target}_defconfig']
                 config_out = io.StringIO()
 
-                # Remove any output targets. Since we use a build directory that
-                # was previously used by another board, it may have produced an
-                # SPL image. If we don't remove it (i.e. see do_config and
-                # self.mrproper below) then it will appear to be the output of
-                # this build, even if it does not produce SPL images.
-                for elf in BASE_ELF_FILENAMES:
-                    fname = os.path.join(out_dir, elf)
-                    if os.path.exists(fname):
-                        os.remove(fname)
+                self._remove_old_outputs(out_dir)
 
                 # If we need to reconfigure, do that now
                 cfg_file = os.path.join(out_dir, '.config')
