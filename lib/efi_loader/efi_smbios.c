c6538008af47a78197a72cb84157643d223266aa
Simon Glass <sjg@chromium.org>
2023-07-15 21:39:17 -0600

    efi: x86: Use the installed SMBIOS tables
    U-Boot sets up the SMBIOS tables during startup. Rather than creating a
    new set, install the existing ones. Rely on the ACPI table's memory-map
    record to cover the tables.
    
    Tidy up the installation-condition code while we are here.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- lib/efi_loader/efi_smbios.c
+++ lib/efi_loader/efi_smbios.c
@@ -20,36 +20,13 @@
  */
 efi_status_t efi_smbios_register(void)
 {
-	/* Map within the low 32 bits, to allow for 32bit SMBIOS tables */
-	u64 dmi_addr = U32_MAX;
-	efi_status_t ret;
-	void *dmi;
+	ulong addr;
 
-	/* Reserve 4kiB page for SMBIOS */
-	ret = efi_allocate_pages(EFI_ALLOCATE_MAX_ADDRESS,
-				 EFI_RUNTIME_SERVICES_DATA, 1, &dmi_addr);
+	/* space for all tables is marked in efi_acpi_register() */
+	addr = gd->arch.smbios_start;
+	printf("EFI using SMBIOS tables at %lx\n", addr);
 
-	if (ret != EFI_SUCCESS) {
-		/* Could not find space in lowmem, use highmem instead */
-		ret = efi_allocate_pages(EFI_ALLOCATE_ANY_PAGES,
-					 EFI_RUNTIME_SERVICES_DATA, 1,
-					 &dmi_addr);
-
-		if (ret != EFI_SUCCESS)
-			return ret;
-	}
-
-	/*
-	 * Generate SMBIOS tables - we know that efi_allocate_pages() returns
-	 * a 4k-aligned address, so it is safe to assume that
-	 * write_smbios_table() will write the table at that address.
-	 */
-	assert(!(dmi_addr & 0xf));
-	dmi = (void *)(uintptr_t)dmi_addr;
-	if (write_smbios_table(map_to_sysmem(dmi)))
-		/* Install SMBIOS information as configuration table */
-		return efi_install_configuration_table(&smbios_guid, dmi);
-	efi_free_pages(dmi_addr, 1);
-	log_err("Cannot create SMBIOS table\n");
-	return EFI_SUCCESS;
+	/* Install SMBIOS information as configuration table */
+	return efi_install_configuration_table(&smbios_guid,
+					       map_sysmem(addr, 0));
 }
